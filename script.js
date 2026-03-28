32c42dab1c339273ffad6e6c518323740345a407
felipersmendes <felipersmendes@hotmail.com>
2026-03-28 20:35:18 -0300

    entendendo e escrevendo README

---
+++ script.js
@@ -0,0 +1,10 @@
+alert('Funcionando');
+let nome = prompt('qual o seu nome, Fofinha?');
+alert('olá pessoa que eu amo muito e se chama: ' + nome);
+alert('Vamos jogar um jogo de advinhação!');
+let adivinhou = prompt('o que você acha que eu vou beber agora?');
+if (adivinhou === 'café') {
+    alert('Parabéns,você acertou!');
+}else {
+    alert('ops, que pena, você errou!eu vou beber café agora!');
+}
